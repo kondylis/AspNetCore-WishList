7fd4741c1999e7ad567b814d1926bdc9631958ca
dko-ucn <60140252+dko-ucn@users.noreply.github.com>
2021-09-17 10:54:30 +0200

    errors

--- WishList/Startup.cs
+++ WishList/Startup.cs
@@ -19,7 +19,7 @@ namespace WishList
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddMvc();
-            services.AddDbContext<ApplicationDbContext>(options =>options.UseInMemoryDatabase("Wishlist"));
+            services.AddDbContext<ApplicationDbContext>(options => options.UseInMemoryDatabase("Wishlist"));
 
         }
 
