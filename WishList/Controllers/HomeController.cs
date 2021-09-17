c156cc3252fc1d4999543c5aca810e349ea6f483
dko-ucn <60140252+dko-ucn@users.noreply.github.com>
2021-09-17 10:29:49 +0200

    refactor homecontroller

--- WishList/Controllers/HomeController.cs
+++ WishList/Controllers/HomeController.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 
-namespace WishList.Controllers.HomeController
+namespace WishList.Controllers
 {
     public class HomeController : Controller
     {
